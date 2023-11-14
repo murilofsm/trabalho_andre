5b0ff6459476dd893203295917ba05c87ae57a68
Emanuelle de Branco <emanuelle.debranco@outlook.com>
2023-11-13 22:08:32 -0300

    add database connection

---
+++ SistemaAcademicoIntefaceGrafica/src/persistencia/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static Connection con = null;
+  
+    private static void criarConexao()
+    {
+        String url = "jdbc:mysql://localhost:3306/atvcadastroaluno";
+        String user = "root";
+        String pass = "010821";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
+        }
+        catch (ClassNotFoundException | SQLException e) {   
+            System.out.println("Falha ao criar conexao com o banco de dados\n"+e.getMessage());
+        }
+    }
+
+    public static Connection getConnection()
+    {
+        try {
+            if(con==null || con.isClosed()){
+                criarConexao();
+            }
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("Falha ao obter conexao\n"+ex.getMessage());
+            return null;
+        }
+    }
+}
+
